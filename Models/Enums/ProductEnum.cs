249f20dfa5341e94258e98ce943ead041c2aef07
Isabelle Gustafsson <isabellegustafsson@Isabelles-MacBook-Air.local>
2024-02-21 11:15:04 +0100

    Moved enum to its own folder

---
+++ Models/Enums/ProductEnum.cs
@@ -0,0 +1,17 @@
+namespace ProductEnum;
+
+public enum Categories
+{
+    Romance,
+    Fantasy,
+    Mystery,
+    Horror,
+    Historical,
+    Thriller,
+    Science,
+    Classics,
+    Biographies,
+    SelfHelp,
+    Poetry,
+    Drama
+}

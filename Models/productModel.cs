d4e5cd4706746596a8db7e8ffe64718a3c8f412a
Isabelle Gustafsson <isabellegustafsson@Isabelles-MacBook-Air.local>
2024-02-21 10:53:45 +0100

    Added categoties as an emun

--- Models/productModel.cs
+++ Models/productModel.cs
@@ -1,21 +1,40 @@
-namespace ProductModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class Product
+namespace ProductModel
 {
-    public int ProductId { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public string Category { get; set; }
-    public string Description { get; set; }
-    public double Price { get; set; }
-
-    public Product(int productId, string title, string author, string category, string description, double price)
+    public class Product
     {
-        ProductId = productId;
-        Title = title;
-        Author = author;
-        Category = category;
-        Description = description;
-        Price = price;
+        public int ProductId { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Categories Category { get; set; } // Change type to Categories enum
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public Product(int productId, string image, string title, string author, Categories category, string description, double price)
+        {
+            ProductId = productId;
+            Image = image;
+            Title = title;
+            Author = author;
+            Category = category;
+            Description = description;
+            Price = price;
+        }
+    }
+
+    public enum Categories
+    {
+        Romance,
+        Fantasy,
+        Mystery,
+        Horror,
+        Historical,
+        Thriller,
+        ScienceFiction,
+        Classics,
+        LiteraryFiction,
+        Biographies
     }
 }
--- Models/userModel.cs
+++ Models/userModel.cs
@@ -1,11 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
 namespace UserModel;
 
-public class User
+public class User:IdentityUser
 {
+    [Key]
     public int UserId { get; set; }
     public string Email { get; set; }
     public string Username { get; set; }
-    public string Password { get; set; }
+    public string? Password { get; set; }
 
     public User(int userId, string email, string username, string password)
     {

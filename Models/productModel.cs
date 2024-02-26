07181d3b7adac5a8fb54ab0783c7237eb5d6f3bd
DaniellaIsaksson <daniellas.isaksson@gmail.com>
2024-02-26 13:28:33 +0100

    Added Categories to productmodel

--- Models/productModel.cs
+++ Models/productModel.cs
@@ -1,24 +1,25 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using ProductEnum;
 
 namespace ProductModel
 {
     public class Product
     {
         public int ProductId { get; set; }
-       // public string Image { get; set; }
+        public string Image { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
-        //public Categories Category { get; set; } // Change type to Categories enum
+        public Categories Category { get; set; } // Change type to Categories enum
         public string Description { get; set; }
         public double Price { get; set; }
 
-        public Product(int productId, string title, string author, string description, double price)
+        public Product(int productId, string image, string title, string author, Categories category, string description, double price)
         {
             ProductId = productId;
-           // Image = image;
+            Image = image;
             Title = title;
             Author = author;
-           // Category = category;
+            Category = category;
             Description = description;
             Price = price;
         }

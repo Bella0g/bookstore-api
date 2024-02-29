fc2d3311353e7ca84d829203279d18ebc8c7456f
DaniellaIsaksson <daniellas.isaksson@gmail.com>
2024-02-29 13:06:15 +0100

    Added logic to increase Quantity of Products in the cart

--- Services/CartService.cs
+++ Services/CartService.cs
@@ -11,30 +11,44 @@ namespace book_store.Services
         {
             _context = context;
         }
-        public async Task<bool> AddProductToCart(int productId, string userId)
+        public async Task<bool> AddProductToCart(int productId, string userId, int quantity = 1)
         {
             var product = await _context.Product.FindAsync(productId);
             var user = await _context.Users.FindAsync(userId);
 
             if (product != null && user != null)
             {
-                var cartItem = new Cart
+                // Checking if the product exists in cart for the current user.
+                var existingCartItem = _context.Cart
+                    .FirstOrDefault(c => c.ProductId == productId && c.UserId == userId);
+
+                if (existingCartItem != null)
                 {
-                    Product = product,
-                    User = user,
-                    Image = product.Image,
-                    Title = product.Title,
-                    Quantity = 1,
-                    TotalPrice = product.Price
-                };
+                    // if product exists, the quantity increase with 1.
+                    existingCartItem.Quantity += quantity;
+                }
+                else
+                {
+                    // If the product does'nt exist, adds a new product to the cart.
+                    var cartItem = new Cart
+                    {
+                        Product = product,
+                        User = user,
+                        Image = product.Image,
+                        Title = product.Title,
+                        Quantity = quantity,
+                        TotalPrice = product.Price * quantity
+                    };
 
-                _context.Cart.Add(cartItem);
+                    _context.Cart.Add(cartItem);
+                }
+
                 await _context.SaveChangesAsync();
-
                 return true;
             }
 
             return false;
         }
+
     }
 }
